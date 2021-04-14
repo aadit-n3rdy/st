61752bf02c9bfce6edc9f1ed9298e11212bb86e6
aadit-n3rdy <eaadithyaa@gmail.com>
2021-04-14 14:37:36 +0530

    To Gentoo

--- .ccls-cache/@home@n3rdy@st/config.h
+++ .ccls-cache/@home@n3rdy@st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "mononoki Nerd Font Mono:pixelsize=13:antialias=true:autohint=true";
+static char *font = "mononoki Nerd Font Mono:pixelsize=27:antialias=true:autohint=true";
 static int borderpx = 5;
 
 /*
