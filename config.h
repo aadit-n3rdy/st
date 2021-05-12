f2e85c75433cf6c36e9d8557390919c70bcd547c
Linux User <n3rdy@alpine.lan>
2021-05-13 00:34:31 +0530

    Changed to nord

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "mononoki Nerd Font Mono:pixelsize=13:antialias=true:autohint=true";
+static char *font = "iosevka Nerd Font Mono:pixelsize=13:antialias=true:autohint=true";
 static int borderpx = 5;
 
 /*
@@ -124,7 +124,7 @@ float alpha = 0.8;
 
 
 // Manually patched dracula colours
-static const char *colorname[] = {
+/*static const char *colorname[] = {
 	"#000000",
 	"#ff5555",
 	"#50fa7b",
@@ -145,7 +145,36 @@ static const char *colorname[] = {
 
 	[256] = "#282a36",
 	[257] = "#f8f8f2",
-};
+};*/
+
+ static const char *colorname[] = {
+ 	/* 8 normal colors */
+	"#3b4252", /* black   */
+	"#bf616a", /* red     */
+	"#a3be8c", /* green   */
+	"#ebcb8b", /* yellow  */
+	"#81a1c1", /* blue    */
+	"#b48ead", /* magenta */
+	"#88c0d0", /* cyan    */
+	"#e5e9f0", /* white   */
+ 
+ 	/* 8 bright colors */
+	"#4c566a", /* black   */
+	"#bf616a", /* red     */
+	"#a3be8c", /* green   */
+	"#ebcb8b", /* yellow  */
+	"#81a1c1", /* blue    */
+	"#b48ead", /* magenta */
+	"#8fbcbb", /* cyan    */
+	"#eceff4", /* white   */
+ 
+ 	[255] = 0,
+ 
+ 	/* more colors can be added after 255 to use with DefaultXX */
+	"#2e3440", /* background */
+	"#d8dee9", /* foreground */
+ };
+
 
 
 /*
@@ -155,7 +184,7 @@ static const char *colorname[] = {
 unsigned int defaultfg = 257;
 unsigned int defaultbg = 256;
 static unsigned int defaultcs = 257;
-static unsigned int defaultrcs = 257;
+static unsigned int defaultrcs = 256;
 
 /*
  * Default shape of cursor
